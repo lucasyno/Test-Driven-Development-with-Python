4d5c787b96c336e8b3f640d108edc891a98e0526
lucasyno <motolucasyno@gmail.com>
2016-03-28 09:53:17 +0200

    Dodanie funkcji pomocniczej do testu funkcjonalnego w celu pozbycia się powielającego kodu

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edyta dowiedziała się o nowej, wspaniałej aplikacji w postaci listy rzeczy do zrobienia
         # Postanowiła więc przejść na stronę główną tej aplikacji
@@ -36,9 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # Po naciśnięciu klawisza Enter strona została uaktualniona i wyświetla
         # '1: Kupić pawie pióra' jako element listy rzeczy do zrobienia
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Kupić pawie pióra')
 
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
         # Edyta wpisałą 'Użyć pawich piór do zrobienia przynęty' (Edyta jest nezwykle skrupulatna)
@@ -47,9 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Użyć pawich piór do zrobienia przynęty', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Kupić pawie pióra')
+        self.check_for_row_in_list_table('2: Użyć pawich piór do zrobienia przynęty')
 
         # Edyta była ciekawa czy strona zapamięta jej listę. Zwróciła uwagę na wygenerowany dla niej
         # unikatowy adres URL, obok którego znajduję się pewien tekst z wyjaśnieniem
