d16cd2dd509fad24b5d244bb34e988296e11f841
lucasyno <motolucasyno@gmail.com>
2016-03-28 09:47:42 +0200

    obsługa tylko jednego elementu na liście, kod przed refaktoryzacją

--- functional_test.py
+++ functional_test.py
@@ -38,20 +38,22 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(rows.text == '1. Kupić pawie pióra' for row in rows),
-            'Nowy element nie znajduje się w tabeli'
-        )
-
+        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
 
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
         # Edyta wpisałą 'Użyć pawich piór do zrobienia przynęty' (Edyta jest nezwykle skrupulatna)
-        self.fail('Zakończenie testu!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Użyć pawich piór do zrobienia przynęty')
+        inputbox.send_keys(Keys.ENTER)
 
         # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Użyć pawich piór do zrobienia przynęty', [row.text for row in rows])
 
         # Edyta była ciekawa czy strona zapamięta jej listę. Zwróciła uwagę na wygenerowany dla niej
         # unikatowy adres URL, obok którego znajduję się pewien tekst z wyjaśnieniem
+        self.fail('Zakończenie testu!')
 
         # Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia
 
--- lists/tests.py
+++ lists/tests.py
@@ -10,10 +10,28 @@ class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
+
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'Nowy element listy'
+
+        response = home_page(request)
+
+        self.assertIn('Nowy element listy', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'Nowy element listy'}
+        )
+
+        self.assertEqual(response.content.decode(), expected_html)
+
