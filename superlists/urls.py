feb913958e8dd757cadd5f1d8220d113ae0aa30a
lucasyno <motolucasyno@gmail.com>
2016-05-14 22:16:48 +0200

    Refactoryzacja widokow, zmiany w szablonach i widok dodawania

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view-list'),
+    url(r'^lists/new$', 'lists.views.new_list', name='new-list'),
 
     # url(r'^admin/', include(admin.site.urls)),
 )
