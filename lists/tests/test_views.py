6fdfa9fb890a425c80bc027d5bab7702ae069ba1
lucasyno <motolucasyno@gmail.com>
2016-05-19 21:48:01 +0200

    Refaktoryzacja widoku listy, aby obslugiwala zadania POST dotyczace nowych elementow list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -78,14 +78,11 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-
-class NewItemTest(TestCase):
-
     def test_can_save_a_post_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
-        self.client.post('/lists/%d/add_item' % (correct_list.id,),
+        self.client.post('/lists/%d/' % (correct_list.id,),
                          data={'item_text': 'Nowy element dla istniejącej listy'})
 
         self.assertEqual(Item.objects.count(), 1)
@@ -93,10 +90,11 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, 'Nowy element dla istniejącej listy')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
-        response = self.client.post('/lists/%d/add_item' % (correct_list.id,),
-                         data={'item_text': 'Nowy element dla istniejącej listy'})
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id, ))
+        response = self.client.post('/lists/%d/' % (correct_list.id,),
+                                    data={'item_text': 'Nowy element dla istniejącej listy'})
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
