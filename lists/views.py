942da9184cb8af78ffd9074b5b1134153dbc24da
lucasyno <motolucasyno@gmail.com>
2016-05-20 19:32:10 +0200

    Wykorzystanie get_absolute_url w modelu List dla DRY url w widokach

--- lists/views.py
+++ lists/views.py
@@ -17,7 +17,7 @@ def view_list(request, list_id):
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect('/lists/%d/' % (list_.id,))
+            return redirect(list_)
         except ValidationError:
             error = "Element nie może być pusty"
     return render(request, 'list.html', {'list': list_, 'error': error})
@@ -33,4 +33,4 @@ def new_list(request):
         list_.delete()
         error = "Element nie może być pusty"
         return render(request, 'home.html', {'error': error})
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect(list_)
