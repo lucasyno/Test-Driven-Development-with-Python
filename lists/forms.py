04c6729ceb4c577b1dd4184907046f48172d0861
lucasyno <motolucasyno@gmail.com>
2016-05-20 21:23:39 +0200

    Nowy formularz dla elementow listy

---
+++ lists/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from .models import Item
+
+EMPTY_LIST_ERROR = "Element listy nie może być pusty"
+
+class ItemForm(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': "Wpisz rzecz do zrobienia",
+                'class': 'form-control input-lg'
+            })
+        }
+        error_messages = {
+            'text': {'required': EMPTY_LIST_ERROR}
+        }
---
+++ lists/tests/test_forms.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from ..forms import EMPTY_LIST_ERROR, ItemForm
+
+
+class ItemFormTest(TestCase):
+
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Wpisz rzecz do zrobienia"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
